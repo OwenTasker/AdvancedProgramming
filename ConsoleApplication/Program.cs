2bd70959df71828c43b807dc4821ee111c43e17f
OwenTasker <zgq19duu@uea.ac.uk>
2021-10-06 16:06:54 +0100

    C# parent

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
-
+﻿
 namespace ConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.Write("Enter an arithmetic operation: \n >> ");;
+            var arithmeticOperationInput = System.Console.ReadLine();
+            System.Console.WriteLine("You Wrote: " + arithmeticOperationInput);
+
+
+            var charArrayRepresentationOfArithmeticInput = arithmeticOperationInput.ToCharArray();
+
+            var stringArrayRepresentationOfArithmeticInput = new string[arithmeticOperationInput.Length];
+
+            for(int i = 0; i < stringArrayRepresentationOfArithmeticInput.Length; i++)
+            {
+                stringArrayRepresentationOfArithmeticInput[i] = charArrayRepresentationOfArithmeticInput[i].ToString();
+            }
+
+            //var tokenizedInput = global::Program.Lexer.lexInput(stringArrayRepresentationOfArithmeticInput);
         }
     }
 }
