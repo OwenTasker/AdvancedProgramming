cf10611c022b1d11d384cc93c7bb532793052a90
psb18 <79208417+psb18@users.noreply.github.com>
2021-10-10 15:57:17 +0100

    Write tests for BNF functions

--- Lexer.Tests/ParserTests.fs
+++ Lexer.Tests/ParserTests.fs
@@ -37,10 +37,37 @@ type ParserTests () =
         Assert.That(result, Is.EqualTo[Parser.terminal.Int 1; Parser.terminal.Times; Parser.terminal.Int 5; Parser.terminal.Times; Parser.terminal.Lpar; Parser.terminal.Int 5; Parser.terminal.Plus; Parser.terminal.Int 6; Parser.terminal.Rpar;])
         
     [<Test>]
-    member this.GivenScan_WhenPassedTokensRepresentingInalidExpression_ThenRecordEquivalentTerminalsInSameOrder() =
+    member this.GivenScan_WhenPassedTokensRepresentingInvalidExpression_ThenRecordEquivalentTerminalsInSameOrder() =
         let result = Parser.scan ["1"; ")"; "5"; "*"; "("; "*"; "+"; "6"; ")"] []
         Assert.That(result, Is.EqualTo[Parser.terminal.Int 1; Parser.terminal.Rpar; Parser.terminal.Int 5; Parser.terminal.Times; Parser.terminal.Lpar; Parser.terminal.Times; Parser.terminal.Plus; Parser.terminal.Int 6; Parser.terminal.Rpar;])
 
     [<Test>]
     member this.GivenScan_WhenPassedInvalidToken_ThenRaiseScanerror() =
         Assert.Throws<Parser.Scanerror>(fun () -> Parser.scan ["1a23";] [] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedInvalidExpression_RaiseParseerror() =
+        Assert.Throws<Parser.Parseerror>(fun () -> Parser.expression [Parser.terminal.Times; Parser.terminal.Plus;] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedValidAddition_ReturnEmptyArray() =
+        let result = Parser.expression [Parser.terminal.Int 1; Parser.terminal.Plus; Parser.terminal.Int 5;]
+        Assert.That(result, Is.EqualTo([]))
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedValidMultiplication_ReturnEmptyArray() =
+        let result = Parser.expression [Parser.terminal.Int 1; Parser.terminal.Times; Parser.terminal.Int 5;]
+        Assert.That(result, Is.EqualTo([]))
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedValidNestedExpression_ReturnEmptyArray() =
+        let result = Parser.expression [Parser.terminal.Int 1; Parser.terminal.Times; Parser.terminal.Int 5; Parser.terminal.Times; Parser.terminal.Lpar; Parser.terminal.Int 5; Parser.terminal.Plus; Parser.terminal.Int 6; Parser.terminal.Rpar;]
+        Assert.That(result, Is.EqualTo([]))
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedLeftBracketWithoutRight_RaiseParseError() =
+        Assert.Throws<Parser.Parseerror>(fun () -> Parser.expression [Parser.terminal.Lpar; Parser.terminal.Int 1; Parser.terminal.Plus; Parser.terminal.Int 1] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedRightBracketWithoutLeft_RaiseParseError() =
+        Assert.Throws<Parser.Parseerror>(fun () -> Parser.expression [Parser.terminal.Lpar; Parser.terminal.Int 1; Parser.terminal.Plus; Parser.terminal.Int 1] |> ignore) |> ignore
