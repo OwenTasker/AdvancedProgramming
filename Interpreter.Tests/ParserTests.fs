a0cd26a979b762d701fd2b1e0d443431772c1f32
psb18 <79208417+psb18@users.noreply.github.com>
2021-10-11 20:29:34 +0100

    Write tests for Exponent and Minus

--- Interpreter.Tests/ParserTests.fs
+++ Interpreter.Tests/ParserTests.fs
@@ -18,9 +18,51 @@ type ParserTests () =
         Assert.That(result, Is.EqualTo([]))
         
     [<Test>]
+    member this.GivenExpression_WhenPassedAdditionWithNoPrecedingValue_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Plus; terminal.Int 5;] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedAdditionWithNoSucceedingValue_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Int 1; terminal.Plus;] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedValidSubtraction_ReturnEmptyArray() =
+        let result = expression [terminal.Int 1; terminal.Minus; terminal.Int 5;]
+        Assert.That(result, Is.EqualTo([]))
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedSubtractionWithNoPrecedingValue_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Minus; terminal.Int 5;] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedSubtractionWithNoSucceedingValue_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Int 1; terminal.Minus;] |> ignore) |> ignore
+        
+    [<Test>]
     member this.GivenExpression_WhenPassedValidMultiplication_ReturnEmptyArray() =
         let result = expression [terminal.Int 1; terminal.Times; terminal.Int 5;]
         Assert.That(result, Is.EqualTo([]))
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedMultiplicationWithNoPrecedingValue_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Times; terminal.Int 5;] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedMultiplicationWithNoSucceedingValue_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Int 1; terminal.Times;] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedValidDivision_ReturnEmptyArray() =
+        let result = expression [terminal.Int 1; terminal.Divide; terminal.Int 5;]
+        Assert.That(result, Is.EqualTo([]))
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedDivisionWithNoPrecedingValue_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Divide; terminal.Int 5;] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_WhenPassedDivisionWithNoSucceedingValue_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Int 1; terminal.Divide;] |> ignore) |> ignore
         
     [<Test>]
     member this.GivenExpression_WhenPassedValidNestedExpression_ReturnEmptyArray() =
@@ -34,6 +76,14 @@ type ParserTests () =
     [<Test>]
     member this.GivenExpression_WhenPassedRightBracketWithoutLeft_RaiseParseError() =
         Assert.Throws<Parseerror>(fun () -> expression [terminal.Lpar; terminal.Int 1; terminal.Plus; terminal.Int 1] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_IntegerFollowedByValidBracketedExpression_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Int 5; terminal.Lpar; terminal.Int 5; terminal.Plus; terminal.Int 6; terminal.Rpar;] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenExpression_ValidBracketedExpressionFollowedByInteger_RaiseParseError() =
+        Assert.Throws<Parseerror>(fun () -> expression [terminal.Lpar; terminal.Int 5; terminal.Plus; terminal.Int 6; terminal.Rpar; terminal.Int 5] |> ignore) |> ignore
         
     [<Test>]
     member this.GivenExpression_WhenPassedIntegerWithExponentAndInteger_ReturnEmptyArray() =
