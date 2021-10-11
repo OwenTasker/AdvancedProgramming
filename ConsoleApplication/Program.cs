e71b0731de1526bc28ead2299c5e0786b2708d8e
OwenTasker <zgq19duu@uea.ac.uk>
2021-10-12 00:15:57 +0100

    Added some stuff to the C# Console application but this uncovered a bug, not sure if it is to do with how F# Lists are managed coming from C# or not but detailed comment inside

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,5 +1,8 @@
 ﻿
+using System;
 using System.Collections.Generic;
+using Interpreter;
+using Microsoft.FSharp.Collections;
 
 namespace ConsoleApplication
 {
@@ -7,18 +10,32 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            System.Console.Write("Enter an arithmetic operation: \n >> ");;
-            var rawInput = System.Console.ReadLine();
-            System.Console.WriteLine("You Wrote: " + rawInput);
+            Console.Write("Enter an arithmetic operation: \n >> ");;
+            var rawInput = Console.ReadLine();
+            Console.WriteLine("You Wrote: " + rawInput);
 
             var inputList = new List<string>();
 
-            foreach (char c in rawInput.ToCharArray())
+            //Turn the input string into a list of strings for each char
+            foreach (char c in rawInput)
             {
                 inputList.Add(c.ToString());
             }
+            
+            //Need to turn Generic System.List to fSharpList
+            var fsharpList = ListModule.OfSeq(inputList);
+            
+            //bug   If last value entered is an int type program will error with an ArgumentException, it looks like
+            //      there is something funky going on with how F# interacts with C# -> F# Lists, if a non-int type
+            //      is the last value - the issue does not exist
+            var tokenizedInput = Interpreter.Lexer.lex(fsharpList);
+            var scannedInput = Interpreter.Lexer.scan(tokenizedInput, FSharpList<Util.terminal>.Empty);
+            foreach (var VARIABLE in scannedInput)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            
 
-            //var tokenizedInput = global::Program.Lexer.lexInput(inputList.ToArray());
         }
     }
 }
