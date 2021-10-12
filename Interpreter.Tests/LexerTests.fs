f1a1e2d3c20e6f7743e581467a41a66aece1eba8
psb18 <79208417+psb18@users.noreply.github.com>
2021-10-12 17:14:18 +0100

    Test single digit end issue

--- Interpreter.Tests/LexerTests.fs
+++ Interpreter.Tests/LexerTests.fs
@@ -1,7 +1,8 @@
-﻿module Lexer.Tests.LexerTests
+﻿module Tests.LexerTests
 
 open NUnit.Framework
-open Interpreter
+open Interpreter.Lexer
+open Interpreter.Util
 
 
 [<TestFixture>]
@@ -10,39 +11,44 @@ type LexerTests ()=
     [<Test>]
     member this.GivenScan_WhenPassedPlusSign_ThenRecordPlus() =
         
-        let result = Lexer.scan ["+";] []
-        Assert.That(result, Is.EqualTo[Util.terminal.Plus;])
+        let result = scan ["+";] []
+        Assert.That(result, Is.EqualTo[terminal.Plus;])
         
     [<Test>]
     member this.GivenScan_WhenPassedTimesSign_ThenRecordTimes() =
-        let result = Lexer.scan ["*";] []
-        Assert.That(result, Is.EqualTo[Util.terminal.Times;])
+        let result = scan ["*";] []
+        Assert.That(result, Is.EqualTo[terminal.Times;])
         
     [<Test>]
     member this.GivenScan_WhenPassedLeftBracket_ThenRecordLpar() =
-        let result = Lexer.scan ["(";] []
-        Assert.That(result, Is.EqualTo[Util.terminal.Lpar;])
+        let result = scan ["(";] []
+        Assert.That(result, Is.EqualTo[terminal.Lpar;])
         
     [<Test>]
     member this.GivenScan_WhenPassedRightBracket_ThenRecordRpar() =
-        let result = Lexer.scan [")";] []
-        Assert.That(result, Is.EqualTo[Util.terminal.Rpar;])
+        let result = scan [")";] []
+        Assert.That(result, Is.EqualTo[terminal.Rpar;])
         
     [<Test>]
     member this.GivenScan_WhenPassedNumber_ThenRecordIntNumber() =
-        let result = Lexer.scan ["123";] []
-        Assert.That(result, Is.EqualTo[Util.terminal.Int 123;])
+        let result = scan ["123";] []
+        Assert.That(result, Is.EqualTo[terminal.Int 123;])
         
     [<Test>]
     member this.GivenScan_WhenPassedTokensRepresentingValidExpression_ThenRecordEquivalentTerminalsInSameOrder() =
-        let result = Lexer.scan ["1"; "*"; "5"; "*"; "("; "5"; "+"; "6"; ")"] []
-        Assert.That(result, Is.EqualTo[Util.terminal.Int 1; Util.terminal.Times; Util.terminal.Int 5; Util.terminal.Times; Util.terminal.Lpar; Util.terminal.Int 5; Util.terminal.Plus; Util.terminal.Int 6; Util.terminal.Rpar;])
+        let result = scan ["1"; "*"; "5"; "*"; "("; "5"; "+"; "6"; ")"] []
+        Assert.That(result, Is.EqualTo[terminal.Int 1; terminal.Times; terminal.Int 5; terminal.Times; terminal.Lpar; terminal.Int 5; terminal.Plus; terminal.Int 6; terminal.Rpar;])
         
     [<Test>]
     member this.GivenScan_WhenPassedTokensRepresentingInvalidExpression_ThenRecordEquivalentTerminalsInSameOrder() =
-        let result = Lexer.scan ["1"; ")"; "5"; "*"; "("; "*"; "+"; "6"; ")"] []
-        Assert.That(result, Is.EqualTo[Util.terminal.Int 1; Util.terminal.Rpar; Util.terminal.Int 5; Util.terminal.Times; Util.terminal.Lpar; Util.terminal.Times; Util.terminal.Plus; Util.terminal.Int 6; Util.terminal.Rpar;])
+        let result = scan ["1"; ")"; "5"; "*"; "("; "*"; "+"; "6"; ")"] []
+        Assert.That(result, Is.EqualTo[terminal.Int 1; terminal.Rpar; terminal.Int 5; terminal.Times; terminal.Lpar; terminal.Times; terminal.Plus; terminal.Int 6; terminal.Rpar;])
 
     [<Test>]
     member this.GivenScan_WhenPassedInvalidToken_ThenRaiseScanerror() =
-        Assert.Throws<Util.Scanerror>(fun () -> Lexer.scan ["1a23";] [] |> ignore) |> ignore
+        Assert.Throws<Scanerror>(fun () -> scan ["1a23";] [] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenLex_WhenPassedExpressionEndingInSingleCharacterInt_ThenReturnCorrectTokens() =
+        let result = lex ["1"; "+"; "2";]
+        Assert.That(result, Is.EqualTo(["1"; "+"; "2";]))
