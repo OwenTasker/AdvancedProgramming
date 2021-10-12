6b8b9bc66c46eeb47e29b817450f743dea565a05
OwenTasker <zgq19duu@uea.ac.uk>
2021-10-12 17:22:38 +0100

    Deleted straggler SLN file and updated C# console program

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Interpreter;
 using Microsoft.FSharp.Collections;
 
@@ -10,30 +11,19 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter an arithmetic operation: \n >> ");;
-            var rawInput = Console.ReadLine();
+            Console.Write("Enter an arithmetic operation: \n >> ");
+            // var rawInput = Console.ReadLine();
+            var rawInput = "1*100+1234121";
             Console.WriteLine("You Wrote: " + rawInput);
 
-            var inputList = new List<string>();
-
-            //Turn the input string into a list of strings for each char
-            foreach (char c in rawInput)
-            {
-                inputList.Add(c.ToString());
-            }
+            //Take an input and implicitly turn it into a List of strings
+            var inputList = rawInput.Select(c => c.ToString()).ToList();
             
             //Need to turn Generic System.List to fSharpList
             var fsharpList = ListModule.OfSeq(inputList);
-            
-            //bug   If last value entered is an int type program will error with an ArgumentException, it looks like
-            //      there is something funky going on with how F# interacts with C# -> F# Lists, if a non-int type
-            //      is the last value - the issue does not exist
-            var tokenizedInput = Interpreter.Lexer.lex(fsharpList);
-            var scannedInput = Interpreter.Lexer.scan(tokenizedInput, FSharpList<Util.terminal>.Empty);
-            foreach (var VARIABLE in scannedInput)
-            {
-                Console.WriteLine(VARIABLE);
-            }
+          
+            var tokenizedInput = Lexer.lex(fsharpList);
+            var scannedInput = Lexer.scan(tokenizedInput, FSharpList<Util.terminal>.Empty);
             
 
         }
