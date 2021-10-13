7842e1c66b3b9d12bdab6622505db97323918faf
ruby-moore <qwq19ffu@uea.ac.uk>
2021-10-13 22:56:16 +0100

    Create initial GUI.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -17,12 +17,64 @@ namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// References:
+    /// https://stackoverflow.com/questions/11873378/adding-placeholder-text-to-textbox
+    /// https://stackoverflow.com/questions/34298857/check-whether-a-textbox-is-empty-or-not
+    /// https://stackoverflow.com/questions/19975617/press-enter-in-textbox-to-and-execute-button-command
+    /// https://stackoverflow.com/questions/14598024/make-textbox-uneditable
+    /// https://stackoverflow.com/questions/18260702/textbox-appendtext-not-autoscrolling
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void EnterButtonPress(object sender, RoutedEventArgs e)
+        {
+            if (inputText.Text == "Enter query here..." || String.IsNullOrEmpty(inputText.Text) || String.IsNullOrWhiteSpace(inputText.Text))
+            {
+                // do nothing
+            }
+            else
+            {
+                String input = inputText.Text;
+                consoleText.AppendText(" " + input + "\n");
+                consoleText.ScrollToEnd();
+                inputText.Clear();
+
+                // 1. send query to lexer/parser/executor
+                // 2. put received answer on new line
+
+                consoleText.AppendText(">>");
+                inputText.Text = "Enter query here...";
+            }
+        }
+
+        private void EnterKeyClick(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                EnterButtonPress(this, new RoutedEventArgs());
+                inputText.Clear();
+            }
+        }
+
+        private void InputTextBoxRemovePrompt(object sender, RoutedEventArgs e)
+        {
+            if (inputText.Text == "Enter query here...")
+            {
+                inputText.Text = "";
+            }
+        }
+
+        private void InputTextBoxAddPrompt(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(inputText.Text) || String.IsNullOrWhiteSpace(inputText.Text))
+            {
+                inputText.Text = "Enter query here...";
+            }
         }
     }
 }
