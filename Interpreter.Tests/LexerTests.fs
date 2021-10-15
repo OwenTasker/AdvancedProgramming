e20eeb23f97635263864f6c4f5ac9169836e1d80
OwenTasker <zgq19duu@uea.ac.uk>
2021-10-15 14:44:59 +0100

    Refactored tests into class tests instead of function tests

--- Interpreter.Tests/LexerTests.fs
+++ Interpreter.Tests/LexerTests.fs
@@ -1,12 +1,110 @@
-﻿module Interpreter.Tests.LexerTests.TokenizerTests
+﻿module Interpreter.Tests.LexerTests
+
 
 open NUnit.Framework
-open Interpreter.Util
 open Interpreter.Lexer
+open Interpreter.Util
+
 
 [<TestFixture>]
-type TokenizerTests ()=
+type LexerTests ()=
+     //Scan function Testing
+    [<Test>]
+    member this.GivenScan_WhenPassedPlusSign_ThenRecordPlus() =
+        let result = scan ["+";] []
+        Assert.That(result, Is.EqualTo[terminal.Plus;])
+
+    [<Test>]
+    member this.GivenScan_WhenPassedMinusSign_ThenRecordMinus() =
+        let result = scan ["-";] []
+        Assert.That(result, Is.EqualTo[terminal.Minus;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedExponentSign_ThenRecordExponent() =
+        let result = scan ["^";] []
+        Assert.That(result, Is.EqualTo[terminal.Exponent;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedTimesSign_ThenRecordTimes() =
+        let result = scan ["*";] []
+        Assert.That(result, Is.EqualTo[terminal.Times;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedLeftBracket_ThenRecordLpar() =
+        let result = scan ["(";] []
+        Assert.That(result, Is.EqualTo[terminal.Lpar;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedRightBracket_ThenRecordRpar() =
+        let result = scan [")";] []
+        Assert.That(result, Is.EqualTo[terminal.Rpar;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedDivideSign_ThenRecordDivide() =
+        let result = scan ["/";] []
+        Assert.That(result, Is.EqualTo[terminal.Divide;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedEqualsSign_ThenRecordEquals() =
+        let result = scan ["=";] []
+        Assert.That(result, Is.EqualTo[terminal.Equals;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedNumber_ThenRecordFloatNumber() =
+        let result = scan ["123";] []
+        Assert.That(result, Is.EqualTo[terminal.Float 123.0;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedTwoNumbersInARow_ThenRecordTwoFloats() =
+        let result = scan ["123";"123";] []
+        Assert.That(result, Is.EqualTo[terminal.Float 123.0; terminal.Float 123.0])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedTokensRepresentingValidExpression_ThenRecordEquivalentTerminalsInSameOrder() =
+        let result = scan ["1"; "*"; "5"; "*"; "("; "5"; "+"; "6"; ")"] []
+        Assert.That(result, Is.EqualTo[terminal.Float 1.0; terminal.Times; terminal.Float 5.0; terminal.Times; terminal.Lpar; terminal.Float 5.0; terminal.Plus; terminal.Float 6.0; terminal.Rpar;])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedTokensRepresentingInvalidExpression_ThenRecordEquivalentTerminalsInSameOrder() =
+        let result = scan ["1"; ")"; "5"; "*"; "("; "*"; "+"; "6"; ")"] []
+        Assert.That(result, Is.EqualTo[terminal.Float 1.0; terminal.Rpar; terminal.Float 5.0; terminal.Times; terminal.Lpar; terminal.Times; terminal.Plus; terminal.Float 6.0; terminal.Rpar;])
+
+    [<Test>]
+    member this.GivenScan_WhenPassedSingleWord_ThenRecordSingleWord() =
+        let result = scan ["abc";] []
+        Assert.That(result, Is.EqualTo[terminal.Word "abc"])
+        
+    [<Test>]
+    member this.GivenScan_WhenPassedTwoWordsInARow_ThenRecordTwoWords() =
+        let result = scan ["abc";"abc";] []
+        Assert.That(result, Is.EqualTo[terminal.Word "abc"; terminal.Word "abc"])
     
+    [<Test>]
+    member this.GivenScan_WhenPassedInvalidNumber_ThenRaiseFormatException() =
+        Assert.Throws<System.FormatException>(fun () -> scan ["1a23";] [] |> ignore) |> ignore
+    
+    [<Test>]
+    member this.GivenScan_WhenPassedInvalidLetter_ThenRaiseFormatException() =
+        Assert.Throws<System.FormatException>(fun () -> scan ["a1a23";] [] |> ignore) |> ignore
+    
+    [<Test>]
+    member this.GivenScan_WhenPassedInvalidToken_ThenRaiseScanError() =
+        Assert.Throws<ScanError>(fun () -> scan ["?";] [] |> ignore) |> ignore
+        
+    [<Test>]
+    member this.GivenLexer_WhenPassedEmptyExpression_ReturnTupleOfEmptyLists() =
+        let result = lexer [""]
+        Assert.That(result, Is.EqualTo(([],[])))
+        
+    [<Test>]
+    member this.GivenLexer_WhenPassedValidExpression_ReturnCorrectTuple() =
+        let result = lexer ["1";"0";"+";"1"]
+        Assert.That(result, Is.EqualTo((["10";"+";"1"],[Float 10.0; Plus; Float 1.0])))
+        
+    [<Test>]
+    member this.GivenLexer_WhenPassedInvalidExpression_ThrowTokenizeError() =
+        Assert.Throws<TokenizeError>(fun _ -> lexer["?"] |> ignore) |> ignore
+        
     //Expect Empty Lists
     [<Test>]
     member this.GivenTokenize_WhenPassedEmptyExpression_ThenReturnEmptyList() =
