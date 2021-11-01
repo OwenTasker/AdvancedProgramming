af7a9e77f75ebcdfeb3410ccd4de2b40ed418dc1
psb18 <79208417+psb18@users.noreply.github.com>
2021-11-01 13:41:56 +0000

    Update UI printing

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace WpfApp1
         public void UpdateVariableWindow()
         {
             var keyValuePairs = _environment.ToList();
-            var variablesList = keyValuePairs.Select(pair => new Variable {Name = pair.Key, Value = pair.Value.ToString()});
+            var variablesList = keyValuePairs.Select(pair => new Variable {Name = pair.Key, Value = Util.terminalListToString("", pair.Value)});
             varDisplay.ItemsSource = variablesList;
         }
 
@@ -78,7 +78,7 @@ namespace WpfApp1
                     consoleText.ScrollToEnd();
                     inputText.Clear();
                     var execOutput = Exec.exec(lexerOutput, Util.toMap(_environment));
-                    consoleText.AppendText(execOutput.Item1 + "\n>>");
+                    consoleText.AppendText(Util.terminalListToString("", execOutput.Item1) + "\n>>");
                     _environment = execOutput.Item2;
                     UpdateVariableWindow();
                     inputText.Text = "Enter query here...";
@@ -93,7 +93,7 @@ namespace WpfApp1
                 }
                 catch (Util.ExecError)
                 {
-                    consoleText.AppendText("Expression cannot be executed");
+                    consoleText.AppendText("Expression cannot be executed\n");
                 }
             }
         }
