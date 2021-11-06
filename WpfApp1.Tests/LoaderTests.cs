847f7d4151d8746d9de3897b05926018b5dc7c00
OwenTasker <zgq19duu@uea.ac.uk>
2021-11-06 17:10:02 +0000

    Created Save/Load testing skeletons

---
+++ WpfApp1.Tests/LoaderTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WpfApp1.Tests
+{
+    public class LoaderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
