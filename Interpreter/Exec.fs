40590fe953081fada956d19ff5a331f367b45b9c
psb18 <79208417+psb18@users.noreply.github.com>
2021-11-08 23:21:55 +0000

    Bit of refactoring

--- Interpreter/Exec.fs
+++ Interpreter/Exec.fs
@@ -53,67 +53,58 @@ let performUnaryOperation operator operand =
 /// the correct handling method. Prepends the result to the number stack.
 /// </summary>
 /// 
-/// <param name="opList">A stack of terminals representing operators.</param>
-/// <param name="numList">A stack of Number terminals.</param>
+/// <param name="operator">A terminal representing an operator.</param>
+/// <param name="numStack">A stack of Number terminals.</param>
 ///
 /// <returns>
 /// A tuple containing the tail of the operator stack, and the number stack with the result of the popped operation
 /// prepended to replace used operands.
 /// </returns>
-let performOperation opList numList =
-    match opList with
-    | []
-    | Rpar :: _ 
-    | Lpar :: _ -> raise (ExecError "Execution Error: Parenthesis encountered as an operator.")
-    | UnaryMinus :: tail
-    | UnaryPlus :: tail ->
-        let operator = opList.[0]
-        match numList with
+let performOperation operator numStack =
+    match operator with
+    | Rpar 
+    | Lpar -> raise (ExecError "Execution Error: Parenthesis encountered as an operator.")
+    | UnaryMinus
+    | UnaryPlus ->
+        match numStack with
         | [] -> raise (ExecError "Execution Error: Unary operation called without any remaining operands.")
         | [ Number f; ] ->
-            tail, (performUnaryOperation operator f) :: []
+            (performUnaryOperation operator f) :: []
         | _ ->
-            match numList.[0] with
+            match numStack.[0] with
             | Number f ->
-                tail, ((performUnaryOperation operator f) :: numList.[1 .. ])
+                ((performUnaryOperation operator f) :: numStack.[1 .. ])
             | _ -> raise (ExecError "Execution Error: Number stack contains non-number tokens.")
-    | head :: tail ->
-        match numList with
+    | _ ->
+        match numStack with
         | [] -> raise (ExecError "Execution Error: Binary operation called without any operands.")
         | [ Number _; ] -> raise (ExecError "Execution Error: Binary operation called with only one operand.")
         | [ Number f; Number g; ] ->
-            let operand1 = f
-            let operand2 = g
-            tail, ((performBinaryOperation head operand2 operand1) :: [])
+            ((performBinaryOperation operator f g) :: [])
         | _ ->
-            match numList.[0], numList.[1] with
+            match numStack.[0], numStack.[1] with
             | Number f, Number g ->
-                let operand1 = f
-                let operand2 = g
-                tail, ((performBinaryOperation head operand2 operand1) :: numList.[2 .. ])
+                ((performBinaryOperation operator f g) :: numStack.[2 .. ])
             | _ -> raise (ExecError "Execution Error: Number stack contains non-number tokens.")
 
 /// <summary>
 /// Recursively calls perform operation until a terminal representing a left parenthesis is encountered.
 /// </summary>
 /// 
-/// <param name="opList"></param>
-/// <param name="numList"></param>
+/// <param name="opStack">A stack </param>
+/// <param name="numStack"></param>
 ///
 /// <returns></returns>
-let rec evaluateBrackets opList numList =
-    match opList with
+let rec evaluateBrackets opStack numStack =
+    match opStack with
     | []
     | Rpar :: _ -> raise (ExecError "Execution Error: Parenthesis encountered as an operator.")
     | Lpar :: tail ->
-        match numList with
-        | _ :: _ -> tail, numList
+        match numStack with
+        | _ :: _ -> tail, numStack
         | [] -> raise (ExecError "Execution Error: Empty number stack following evaluation of bracketed expression.")
-    | _ ->
-        let results = performOperation opList numList
-        match results with
-        | opList, numList ->
-            evaluateBrackets opList numList
+    | head :: tail ->
+        evaluateBrackets tail (performOperation head numStack)
 
 /// <summary>
 /// Map containing the precedence and associativity of operators accepted by the performUnaryOperation and
@@ -154,30 +145,28 @@ let getAssociativity operator =
 /// associativity rather than producing a reverse Polish notation output.
 /// </summary>
 ///
-/// <param name="tokens"></param>
-/// <param name="opList"></param>
-/// <param name="numList"></param>
+/// <param name="terminals"></param>
+/// <param name="opStack"></param>
+/// <param name="numStack"></param>
 /// <param name="env"></param>
 ///
 /// <returns></returns>
-let rec reduceRecursive tokens opList numList (env: Map<string, terminal list>) =
-    match tokens with
-    | tokenHead :: tokenTail ->
-        match tokenHead with
+let rec reduceRecursive terminals opStack numStack (env: Map<string, terminal list>) =
+    match terminals with
+    | terminalHead :: terminalTail ->
+        match terminalHead with
         | Number _ ->
-            reduceRecursive tokenTail opList (tokenHead :: numList) env
+            reduceRecursive terminalTail opStack (terminalHead :: numStack) env
         | Word x ->
-            if env.ContainsKey x then
-                let value = reduce env.[x] env
-                reduceRecursive tokenTail opList (value :: numList) env
+            if env.ContainsKey x
+            then reduceRecursive terminalTail opStack (reduce env.[x] env :: numStack) env
             else raise (ExecError "Execution Error: Expression with unbound variables passed to reduce.")
         | Lpar ->
-            reduceRecursive tokenTail (tokenHead :: opList) numList env
+            reduceRecursive terminalTail (terminalHead :: opStack) numStack env
         | Rpar ->
-            let results = evaluateBrackets opList numList
-            match results with
-            opList, numList ->
-                reduceRecursive tokenTail opList numList env
+            match evaluateBrackets opStack numStack with
+            opStack, numStack ->
+                reduceRecursive terminalTail opStack numStack env
         | UnaryMinus
         | UnaryPlus  
         | Divide
@@ -185,45 +174,42 @@ let rec reduceRecursive tokens opList numList (env: Map<string, terminal list>) 
         | Minus
         | Plus
         | Exponent ->
-            match opList with
+            match opStack with
             | [] ->
-                reduceRecursive tokenTail (tokenHead :: opList) numList env
-            | opHead :: _ ->
+                reduceRecursive terminalTail (terminalHead :: opStack) numStack env
+            | opHead :: opTail ->
                 match opHead with
                 | Lpar ->
-                    reduceRecursive tokenTail (tokenHead :: opList) numList env
+                    reduceRecursive terminalTail (terminalHead :: opStack) numStack env
                 | _ ->
-                    if (getPrecedence tokenHead < getPrecedence opHead
-                        || getPrecedence tokenHead = getPrecedence opHead && getAssociativity tokenHead = "l") then
-                        let results = performOperation opList numList
-                        match results with
-                        opList, numList ->
-                            reduceRecursive tokens opList numList env
-                    else reduceRecursive tokenTail (tokenHead :: opList) numList env
+                    if (getPrecedence terminalHead < getPrecedence opHead
+                        || getPrecedence terminalHead = getPrecedence opHead
+                           && getAssociativity terminalHead = "l")
+                    then reduceRecursive terminals opTail (performOperation opHead numStack) env
+                    else reduceRecursive terminalTail (terminalHead :: opStack) numStack env
         | _ -> raise (ExecError "Execution Error: Invalid terminal passed to reduce.")
     | [] ->
-        match opList with
+        match opStack with
         | [] ->
-            match numList with
-            | [ _ ] -> numList.[0]
+            match numStack with
+            | [ _ ] -> numStack.[0]
             | _ -> raise (ExecError "Execution Error: Reduction did not result in exactly one terminal in the number
                           stack")
-        | Lpar :: _ -> raise (ExecError "Execution Error: Unmatched left parenthesis.")
-        | _ ->
-            let results = performOperation opList numList
-            match results with
-            | opList, numList -> reduceRecursive tokens opList numList env
+        | Lpar :: _ ->
+            raise (ExecError "Execution Error: Unmatched left parenthesis.")
+        | head :: tail ->
+            reduceRecursive terminals tail (performOperation head numStack) env
 
 /// <summary>
 /// Wrapper for reduceRecursive to call it with empty operator and number stacks.
 /// </summary>
 ///
-/// <param name="tokens"></param>
+/// <param name="terminals"></param>
 /// <param name="env"></param>
 ///
 /// <returns></returns>
-and reduce tokens (env: Map<string, terminal list>) =
-    reduceRecursive tokens [] [] env
+and reduce terminals (env: Map<string, terminal list>) =
+    reduceRecursive terminals [] [] env
 
 /// <summary>
 /// Checks whether a function contains any variables whose values are not defined in the current environment.
@@ -237,7 +223,9 @@ let rec closed terminals (env: Map<string, terminal list>) =
     match terminals with
     | [] -> true
     | Word x :: tail ->
-        if env.ContainsKey x && closed env.[x] env then closed tail env else false
+        if env.ContainsKey x && closed env.[x] env
+        then closed tail env
+        else false
     | _ :: tail -> closed tail env
 
 /// <summary>
@@ -251,8 +239,8 @@ let rec closed terminals (env: Map<string, terminal list>) =
 let rec createTerminalListUpToComma inList outList =
     match inList with
     | Rpar :: _ -> (inList, List.rev outList)
-    | Comma :: tail -> (tail, List.rev outList)
-    | any :: tail -> createTerminalListUpToComma tail (any :: outList)
+    | Comma :: inTail -> (inTail, List.rev outList)
+    | any :: inTail -> createTerminalListUpToComma inTail (any :: outList)
     | [] -> raise (ExecError "Execution Error: Function call missing right parenthesis.")
 
 /// <summary>
@@ -265,8 +253,7 @@ let rec createTerminalListUpToComma inList outList =
 /// <returns></returns>
 let rec setArguments terminals (env: Map<string, terminal list>) =
     match terminals with
-    | Rpar :: _ ->
-        env
+    | Rpar :: _ -> env
     | Word x :: Assign :: tail ->
         match createTerminalListUpToComma tail [] with
         | a, b -> setArguments a (env.Add(x, [reduce b env] ))
@@ -284,19 +271,21 @@ let rec setArguments terminals (env: Map<string, terminal list>) =
 let exec terminals (env: Map<string, terminal list>) =
     match terminals with
     | Word x :: Assign :: tail ->
-        if closed tail env then 
+        if closed tail env
+        then
             let result = [reduce tail env]
-        //https://stackoverflow.com/questions/27109142/f-map-to-c-sharp-dictionary/27109303
+            //https://stackoverflow.com/questions/27109142/f-map-to-c-sharp-dictionary/27109303
             result, (env.Add(x, result) |> Map.toSeq |> dict)
         else terminals, (env.Add(x, tail) |> Map.toSeq |> dict) 
     | Word x :: Lpar :: tail ->
         //https://stackoverflow.com/questions/3974758/in-f-how-do-you-merge-2-collections-map-instances
         let newEnv = setArguments tail Map.empty
         let combinedEnv = Map.fold (fun acc key value -> Map.add key value acc) env newEnv
-        if closed [Word x] combinedEnv then 
-            [reduce [Word x] combinedEnv], (env |> Map.toSeq |> dict)
+        
+        if closed [Word x] combinedEnv
+        then [reduce [Word x] combinedEnv], (env |> Map.toSeq |> dict)
         else raise (ExecError "Execution Error: Assignments given in function call do not close expression.")
     | _ ->
-        if closed terminals env then
-            [reduce terminals env], (env |> Map.toSeq |> dict)
+        if closed terminals env
+        then [reduce terminals env], (env |> Map.toSeq |> dict)
         else terminals, (env |> Map.toSeq |> dict)
