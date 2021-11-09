d16e14135211ee773e918014a428444393cb0f3a
psb18 <79208417+psb18@users.noreply.github.com>
2021-11-09 21:15:55 +0000

    Add differentiate to function list

--- Interpreter/Util.fs
+++ Interpreter/Util.fs
@@ -48,6 +48,7 @@ let functions = [
                  ("cbrt", "One Argument; A function to determine the cube root of a value, given a value of 8, will return 2")
                  ("round", "One Argument; A function to determine the rounded value of the provided argument, given a value of 2.5, will return 3")
                  ("plot", "")
+                 ("differentiate", "One Argument; A function to differentiate an expression provided as an argument, given a value of x^2, will return 2*x")
                  ]
 
 //https://gist.github.com/theburningmonk/3363893
