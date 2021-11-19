9fd780379c567b148dcb6a48e3fdc93927d91dee
OwenTasker <zgq19duu@uea.ac.uk>
2021-11-19 11:25:48 +0000

    Moved InvalidArgumentError to util.fs
    Created basic testing for logE valid cases
    
    Added handling within taylor series for LogE values 0<x<=0.5
    
    Restructured files slightly to be matching in both testing and interpreter

---
+++ Interpreter.Tests/MathematicalFunctionsTests.fs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// Module containing tests for the functions defined in Interpreter.MathematicalFunctions.fs.
+/// </summary>
+///
+/// <namespacedoc>
+///     <summary>Interpreter.Tests</summary>
+/// </namespacedoc>
+module Interpreter.Tests.MathematicalFunctionsTests
+
+open NUnit.Framework
+open Interpreter.MathematicalFunctions
+
+let LogEInputsOutputs = [
+    TestCaseData(0.25, -1.386294)
+    TestCaseData(0.5, -0.693147)
+    TestCaseData(0.6, -0.510825)
+    TestCaseData(0.75, -0.287682)
+    TestCaseData(0.9, -0.105360)
+    TestCaseData(1.0, 0.0)
+    TestCaseData(1.5, 0.405465)
+    TestCaseData(2.0, 0.693147)
+    TestCaseData(2.718281, 1.0)
+    TestCaseData(3.0, 1.098612)
+    TestCaseData(5.0, 1.609437)
+    TestCaseData(10.0, 2.302585)
+]
+
+[<TestCaseSource("LogEInputsOutputs")>]
+let givenLogE_ProvidedValidInput_ReturnCorrectApproximation input output =
+    let logRes = LogE' input
+    let a = logRes > (output-0.000001)
+    let b = logRes < (output+0.000001)
+    Assert.True(a && b)
--- Interpreter/MathematicalFunctions.fs
+++ Interpreter/MathematicalFunctions.fs
@@ -1,20 +1,31 @@
 ﻿module Interpreter.MathematicalFunctions
 
-exception InvalidArgumentException of string
-exception DomainException of string
+open System
+open Interpreter.Util
 
 //Reference :: https://www.efunda.com/math/taylor_series/logarithmic.cfm
-let rec LogE (input:float) (increment:float) (sum:float) =
+let rec LogEGreaterThanZeroPointFive (input:float) (increment:float) (sum:float) =
     match increment with
     | 200.0 -> sum
-    | _  -> LogE input (increment+1.0) (sum+(1.0/increment)*((input-1.0)/input)**increment)
+    | _  -> LogEGreaterThanZeroPointFive input (increment+1.0) (sum+(1.0/increment)*((input-1.0)/input)**increment)
+
+let rec LogELessThanOrEqualToZeroPointFive (input:float) (increment:float) (sum:float) =
+    match increment with
+    | 200.0 -> sum
+    | _ ->
+        match (increment%2.0) with
+        | 0.0 -> LogELessThanOrEqualToZeroPointFive input (increment+1.0) sum-(((input-1.0)**increment)/increment)
+        | 1.0 -> LogELessThanOrEqualToZeroPointFive input (increment+1.0) sum+(((input-1.0)**increment)/increment)
+        | _ -> raise (InvalidArgumentError "Invalid Argument Error: Increment Not A Whole Number")
 
 let LogE' input =
-    if input = 0.0 then
-        raise (InvalidArgumentException "")
+    if input <= 0.0 then
+        raise (InvalidArgumentError "Input To LogE Can Not Be 0")
+    elif input <= 0.5 then
+        LogELessThanOrEqualToZeroPointFive input 1.0 0.0
     else
-        LogE input 1.0 0.0
-    
+        LogEGreaterThanZeroPointFive input 1.0 0.0
+        
 let formNewBaseRuleFraction numerator denominator =
     LogE' numerator / LogE' denominator
 
