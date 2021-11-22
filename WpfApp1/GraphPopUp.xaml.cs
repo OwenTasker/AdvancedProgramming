cc00ec0b3972c9fadecc6d810469aa5df83a311b
Alex-Rattigan <alex@alexrattigan.co.uk>
2021-11-22 00:33:13 +0000

    initial text over image

--- WpfApp1/GraphPopUp.xaml.cs
+++ WpfApp1/GraphPopUp.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using Interpreter;
 using Microsoft.FSharp.Collections;
+using Image = System.Drawing.Image;
 
 namespace WpfApp1
 {
@@ -110,6 +111,29 @@ namespace WpfApp1
                     image.Save(path + "MyMathsPal\\graph" + _thisImageId + ".png" );
                 }
             }
+            
+            var firstText = "Hello";
+            var secondText = "World";
+
+            var firstLocation = new PointF(10f, 10f);
+            var secondLocation = new PointF(10f, 50f);
+
+            Bitmap newBitmap;
+            using (var bitmap = (Bitmap)Image.FromFile(path + "MyMathsPal\\graph" + _thisImageId + ".png"))//load the image file
+            {
+                using(var graphics = Graphics.FromImage(bitmap))
+                {
+                    using (var arialFont =  new Font("Arial", 10))
+                    {
+                        graphics.DrawString(firstText, arialFont, Brushes.Blue, firstLocation);
+                        graphics.DrawString(secondText, arialFont, Brushes.Red, secondLocation);
+                    }
+                }
+                newBitmap = new Bitmap(bitmap);
+            }
+
+            newBitmap.Save(path + "MyMathsPal\\graph" + _thisImageId + ".png");//save the image file
+            newBitmap.Dispose();
             
             //Display graph
             var stream = File.OpenRead(path + "MyMathsPal\\graph" + _thisImageId + ".png");
