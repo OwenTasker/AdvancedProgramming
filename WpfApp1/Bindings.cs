0ed9b833ce56d71d2113f266d5aff315fd94891f
psb18 <79208417+psb18@users.noreply.github.com>
2021-11-24 13:41:37 +0000

    Define interface bindings

---
+++ WpfApp1/Bindings.cs
@@ -0,0 +1,16 @@
+﻿using Ninject.Modules;
+
+namespace WpfApp1
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IInterpreter>().To<Interpreter>().InSingletonScope();
+            Bind<IGraphPopUp>().To<GraphPopUp>().InTransientScope();
+            Bind<IGraphDataCalculator>().To<GraphDataCalculator>().InSingletonScope();
+            Bind<ISaverLoader>().To<SaverLoader>().InSingletonScope();
+        }
+        
+    }
+}
