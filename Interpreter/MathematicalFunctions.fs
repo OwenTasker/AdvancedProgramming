96db61a7f587a9d011b4b79dfb2268cd36b0ec0b
psb18 <79208417+psb18@users.noreply.github.com>
2021-11-28 16:34:24 +0000

    Revert change to MathematicalFunctions.fs in "Fix xrt"

--- Interpreter/MathematicalFunctions.fs
+++ Interpreter/MathematicalFunctions.fs
@@ -158,13 +158,13 @@ let LogWrapperToTerminal (logFunction: float->float) logVal=
 /// Returns a list of terminals equal to the value to calculate the root of to the power of 1/denominator
 /// </returns>
 let RootToTerminals (terminals: terminal list) denominator =
-    if not terminals.IsEmpty then
-         if denominator <> 0.0 then
-             [Lpar; Lpar] @ terminals @ [Rpar; Exponent; Lpar; Number 1.0; Divide; Number denominator; Rpar; Rpar]
-         else
-             [Lpar; Number 1.0; Rpar]
+    let terminalsNotEmpty = not terminals.IsEmpty
+    let denominatorNotZeroOrNegative = not (denominator < 0.0)
+    if terminalsNotEmpty && denominatorNotZeroOrNegative then
+        [Lpar; Lpar] @ terminals @ [Rpar; Exponent; Lpar; Number 1.0; Divide; Number denominator; Rpar; Rpar]
     else
-        InvalidArgumentError "Ensure that input value is not empty." |> raise
+        InvalidArgumentError "Ensure that input value is not empty and the root you are taking is not 0" |> raise
+    
     
 /// <summary>
 /// Function to calculate the floor of a number, uses the fact that in F#, downcasting a float to int just truncates it
