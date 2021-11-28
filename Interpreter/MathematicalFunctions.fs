e7da14b4161cc21364ba9d2387204eab1a585986
psb18 <79208417+psb18@users.noreply.github.com>
2021-11-28 16:36:50 +0000

    Update xrt to handle expressions

--- Interpreter/MathematicalFunctions.fs
+++ Interpreter/MathematicalFunctions.fs
@@ -158,10 +158,8 @@ let LogWrapperToTerminal (logFunction: float->float) logVal=
 /// Returns a list of terminals equal to the value to calculate the root of to the power of 1/denominator
 /// </returns>
 let RootToTerminals (terminals: terminal list) denominator =
-    let terminalsNotEmpty = not terminals.IsEmpty
-    let denominatorNotZeroOrNegative = not (denominator < 0.0)
-    if terminalsNotEmpty && denominatorNotZeroOrNegative then
-        [Lpar; Lpar] @ terminals @ [Rpar; Exponent; Lpar; Number 1.0; Divide; Number denominator; Rpar; Rpar]
+    if not terminals.IsEmpty && denominator <> [Number 0.0] then
+        [Lpar; Lpar] @ terminals @ [Rpar; Exponent; Lpar; Number 1.0; Divide] @ denominator @ [Rpar; Rpar]
     else
         InvalidArgumentError "Ensure that input value is not empty and the root you are taking is not 0" |> raise
     
